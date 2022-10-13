c921d3320889485a4f9f25369b5e1a1e5d08596f
ylii0456 <ylii0456@student.monash.edu>
2022-10-13 15:16:59 +1100

    Add project files.

---
+++ FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("ylii0456@student.monash.edu", "FIT5032 Example Email User");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
